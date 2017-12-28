8be2beda9cdce26b0f7f8ca284edbaa772b11bba
Eugene Kuznietsov <Eugene.kuznietsov@gmail.com>
2017-12-28 22:16:40 +0200

    add simple web interface

---
+++ app/controllers/index_controller.rb
@@ -0,0 +1,17 @@
+class IndexController < ApplicationController
+
+  def index
+  end
+
+  def exchange
+    ParseRates.perform
+    @result = Exchanger.exchange(permitted_params[:amount].to_i, permitted_params[:dates].gsub(/\s+/, '').split(','))
+    render :index
+  end
+
+  private
+
+  def permitted_params
+    params.permit(:amount, :dates)
+  end
+end
